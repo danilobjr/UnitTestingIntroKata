c629c715d80e7a970d2813cf57aec3449faffc39
Danilo Barros <danilo.barros@lsbd.ufc.br>
2015-04-14 10:12:52 -0300

    test for RegisterNewUser of AccountManager

---
+++ UnitTestingIntroDemo.Main.Tests/Managers/AccountManagerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestingIntroDemo.Main.Managers;
+using UnitTestingIntroDemo.Main.Models;
+
+namespace UnitTestingIntroDemo.Main.Tests.Managers
+{
+    [TestFixture]
+    public class AccountManagerTests
+    {
+        [Test]
+        public void RegisterNewUser_InvalidNewUser_ReturnsFalse()
+        {
+            // Arrange
+            var invalidUser = new User();
+            var manager = new AccountManager();
+
+            // Act
+            var userWasRegistered = manager.RegisterNewUser(invalidUser);
+
+            // Assert
+            Assert.IsFalse(userWasRegistered);
+        }
+    }
+}
