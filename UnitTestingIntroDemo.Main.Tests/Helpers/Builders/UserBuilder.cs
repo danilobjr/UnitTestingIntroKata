cd2feb7a727597493831e91e33108c89c2fc1602
Danilo Barros <danilo.barros@lsbd.ufc.br>
2015-04-15 18:05:36 -0300

    tests for UserValidator

---
+++ UnitTestingIntroDemo.Main.Tests/Helpers/Builders/UserBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestingIntroDemo.Main.Models;
+
+namespace UnitTestingIntroDemo.Main.Tests.Helpers.Builders
+{
+    class UserBuilder
+    {
+        private string _email { get; set; }
+        private string _password { get; set; }
+
+        public static UserBuilder AUser()
+        {
+            return new UserBuilder().WithEmail("a@a.com").WithPassword("abCD12!_");
+        }
+
+        //public UserBuilder Valid()
+        //{
+        //    _email = "a@a.com";
+        //    _password = "abCD12!_";
+
+        //    return this;
+        //}
+
+        public UserBuilder WithEmail(string email)
+        {
+            _email = email;
+
+            return this;
+        }
+
+        public UserBuilder WithPassword(string password)
+        {
+            _password = password;
+
+            return this;
+        }
+
+        public User Build()
+        {
+            return new User
+            {
+                Email = _email,
+                Password = _password
+            };
+        }
+    }
+}
--- UnitTestingIntroDemo.Main.Tests/Validators/UserValidatorTests.cs
+++ UnitTestingIntroDemo.Main.Tests/Validators/UserValidatorTests.cs
@@ -1,10 +1,12 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnitTestingIntroDemo.Main.Models;
+using UnitTestingIntroDemo.Main.Tests.Helpers.Builders;
 using UnitTestingIntroDemo.Main.Validators;
 
 namespace UnitTestingIntroDemo.Main.Tests.Validators
@@ -13,39 +15,142 @@ namespace UnitTestingIntroDemo.Main.Tests.Validators
     public class UserValidatorTests
     {
         [Test]
-        public void IsValid_UserEmailIsNull_ReturnsFalse()
+        public void IsValid_EmailIsNull_ReturnsFalse()
         {
-            // Arrange
-            var invalidUser = new User
-            {
-                Email = null
-            };
+            var user = UserBuilder.AUser()
+                .WithEmail(null)
+                .Build();
 
-            var validator = new UserValidator();
-
-            // Act
-            var userIsVaild = validator.IsValid(invalidUser);
-
-            // Assert
-            Assert.IsFalse(userIsVaild);
+            AssertThat(user, expectation: Is.False);
         }
 
         [Test]
-        public void IsValid_UserEmailIsEmpty_ReturnsFalse()
+        public void IsValid_EmailIsEmpty_ReturnsFalse()
         {
-            // Arrange
-            var invalidUser = new User
-            {
-                Email = ""
-            };
+            var user = UserBuilder.AUser()
+                .WithEmail("")
+                .Build();
 
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_EmailIsInvalid_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithEmail("a@a")
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_PasswordIsNull_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithPassword(null)
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_PasswordIsEmpty_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithPassword("")
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_PasswordWithLessThan6Characters_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithPassword("12aB_")
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_PasswordWithMoreThan10Characters_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithPassword("12345678aB_")
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_PasswordWithWhitespace_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithPassword("abCD 12!_")
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_PasswordWithoutLowercaseLetter_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithPassword("ABC123!_")
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_PasswordWithoutUppercaseLetter_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithPassword("abc123!_")
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_PasswordWithoutNumbers_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithPassword("abcDEF!_")
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_PasswordWithoutSymbols_ReturnsFalse()
+        {
+            var user = UserBuilder.AUser()
+                .WithPassword("abCD1234")
+                .Build();
+
+            AssertThat(user, expectation: Is.False);
+        }
+
+        [Test]
+        public void IsValid_ValidEmailAndPassword_ReturnsTrue()
+        {
+            var user = UserBuilder.AUser().Build();
+
+            AssertThat(user, expectation: Is.True);
+        }
+        
+        private static void AssertThat(User user, IResolveConstraint expectation)
+        {
             var validator = new UserValidator();
 
             // Act
-            var userIsVaild = validator.IsValid(invalidUser);
+            var result = validator.IsValid(user);
 
             // Assert
-            Assert.IsFalse(userIsVaild);
-        }
+            Assert.That(result, expectation);
+        }        
     }
 }
